6453835a150cc22d22ad00538c66649179215e3f
sleek1004 <arojojoyeo@yahoo.com>
2023-03-28 12:09:14 +0100

    adding addons pages

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
-
+let paragraphs = document.querySelectorAll(".duration")
 
 
 function togglecheckbox(){
     const checkbox = document.getElementById("toggle-box")
     checkbox.checked = !checkbox.checked;
+
+   
+    let plans = document.querySelectorAll(".plan");
+    let monthlyPrices = ["$9/mo", "$12/mo", "$15/mo"];
+    let yearlyPrices = ["$9/mo", "$120/yearly", "$150/yearly"];
+    
+  
+    if (checkbox.checked) {
+      plans.forEach(function(plan, i) {
+        let planName = plan.querySelector("#planP").textContent;
+        let planPrice = yearlyPrices[i];
+        plan.querySelector("#pay").textContent = planPrice;
+        paragraphs.forEach((paragraph) => {
+            paragraph.style.visibility = "visible";
+          });
+        
+        
+      });
+    } else {
+      plans.forEach(function(plan, i) {
+        let planName = plan.querySelector("#planP").textContent;
+        let planPrice = monthlyPrices[i];
+        plan.querySelector("#pay").textContent = planPrice;
+        paragraphs.forEach((paragraph) => {
+            paragraph.style.visibility = "hidden";
+          });
+        
+      });
+    }
 }
 
 
@@ -19,42 +48,5 @@ let selectedPlan=null;
     })
  })
 
-// Get the toggle box element
-const toggleBox = document.getElementById('toggle-box');
 
-// Add an event listener to the toggle box element
-toggleBox.addEventListener('change', function() {
-
-  // Get the plans container element
-  const plansContainer = document.querySelector('.plans-container');
-
-  // Get all the plan elements
-  const plans = plansContainer.querySelectorAll('.plan');
-
-  // Check if the toggle is checked (yearly billing)
-  if (toggleBox.checked) {
-    
-    // Change the plan names and prices for yearly billing
-    plans[0].querySelector('#planP').textContent = 'Arcade (Yearly)';
-    plans[0].querySelector('#pay').textContent = '$90/year';
-    
-    plans[1].querySelector('#planP').textContent = 'Advanced (Yearly)';
-    plans[1].querySelector('#pay').textContent = '$120/year';
-    
-    plans[2].querySelector('#planP').textContent = 'Pro (Yearly)';
-    plans[2].querySelector('#pay').textContent = '$150/year';
   
-  } else {
-  
-    // Change the plan names and prices for monthly billing
-    plans[0].querySelector('#planP').textContent = 'Arcade';
-    plans[0].querySelector('#pay').textContent = '$9/mo';
-    
-    plans[1].querySelector('#planP').textContent = 'Advanced';
-    plans[1].querySelector('#pay').textContent = '$12/mo';
-    
-    plans[2].querySelector('#planP').textContent = 'Pro';
-    plans[2].querySelector('#pay').textContent = '$15/mo';
-  
-  }
-});
