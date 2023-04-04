7766a92900e639d23819fdc86b2c2d3afe64f0d2
sleek1004 <arojojoyeo@yahoo.com>
2023-04-04 23:52:50 +0100

    added evenlistener to the next button

--- index.js
+++ index.js
@@ -1,52 +1,67 @@
-let paragraphs = document.querySelectorAll(".duration")
+let paragraphs = document.querySelectorAll(".duration");
 
+function togglecheckbox() {
+  const checkbox = document.getElementById("toggle-box");
+  checkbox.checked = !checkbox.checked;
 
-function togglecheckbox(){
-    const checkbox = document.getElementById("toggle-box")
-    checkbox.checked = !checkbox.checked;
+  let plans = document.querySelectorAll(".plan");
+  let monthlyPrices = ["$9/mo", "$12/mo", "$15/mo"];
+  let yearlyPrices = ["$9/mo", "$120/yearly", "$150/yearly"];
 
-   
-    let plans = document.querySelectorAll(".plan");
-    let monthlyPrices = ["$9/mo", "$12/mo", "$15/mo"];
-    let yearlyPrices = ["$9/mo", "$120/yearly", "$150/yearly"];
-    
-  
-    if (checkbox.checked) {
-      plans.forEach(function(plan, i) {
-        let planName = plan.querySelector("#planP").textContent;
-        let planPrice = yearlyPrices[i];
-        plan.querySelector("#pay").textContent = planPrice;
-        paragraphs.forEach((paragraph) => {
-            paragraph.style.visibility = "visible";
-          });
-        
-        
+  if (checkbox.checked) {
+    plans.forEach(function (plan, i) {
+      let planName = plan.querySelector("#planP").textContent;
+      let planPrice = yearlyPrices[i];
+      plan.querySelector("#pay").textContent = planPrice;
+      paragraphs.forEach((paragraph) => {
+        paragraph.style.visibility = "visible";
       });
-    } else {
-      plans.forEach(function(plan, i) {
-        let planName = plan.querySelector("#planP").textContent;
-        let planPrice = monthlyPrices[i];
-        plan.querySelector("#pay").textContent = planPrice;
-        paragraphs.forEach((paragraph) => {
-            paragraph.style.visibility = "hidden";
-          });
-        
+    });
+  } else {
+    plans.forEach(function (plan, i) {
+      let planName = plan.querySelector("#planP").textContent;
+      let planPrice = monthlyPrices[i];
+      plan.querySelector("#pay").textContent = planPrice;
+      paragraphs.forEach((paragraph) => {
+        paragraph.style.visibility = "hidden";
       });
-    }
+    });
+  }
 }
 
-
 const plans = document.querySelectorAll(".plan");
-let selectedPlan=null;
- plans.forEach(plan =>{
-    plan.addEventListener('click', function(){
-        if (selectedPlan){
-            selectedPlan.classList.remove('selected')
-        }
-        plan.classList.add('selected')
-        selectedPlan = plan
-    })
- })
+let selectedPlan = null;
+plans.forEach((plan) => {
+  plan.addEventListener("click", function () {
+    if (selectedPlan) {
+      selectedPlan.classList.remove("selected");
+    }
+    plan.classList.add("selected");
+    selectedPlan = plan;
+  });
+});
 
+// Get the toggle button and the plans container
+const toggleBtn = document.getElementById("toggle-box");
+const plansContainer = document.querySelector(".plans-container");
 
-  
+const nextBtn = document.getElementById("pagenextbutton");
+
+nextBtn.addEventListener("click", () => {
+  const isYearly = toggleBtn.checked;
+
+  const selectedPlan = plansContainer.querySelector(".selected");
+
+  if (selectedPlan) {
+    const planName = selectedPlan.querySelector("#planP");
+
+    let url;
+    if (isYearly) {
+      url = `/addons.html`;
+    } else {
+      url = `/step3monthly.html`;
+    }
+
+    window.location.href = url;
+  }
+});
