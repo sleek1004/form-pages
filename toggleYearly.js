a88bf116528b881a3e25209c941e654f14be06fa
sleek1004 <arojojoyeo@yahoo.com>
2023-04-26 23:59:06 +0100

    separating page2 of the HTML

--- toggleYearly.js
+++ toggleYearly.js
@@ -1,17 +1,20 @@
 function togglecheckbox() {
     console.log("working");
-    
+    let paragraphs = document.querySelectorAll(".duration");
     const checkBox = document.getElementById("toggle-box");
     checkBox.checked = !checkBox.checked;
     let plans = document.querySelectorAll(".plan");
-   
+    let monthlyPrices = ["$9/mo", "$12/mo", "$15/mo"];
+    let yearlyPrices = ["$9/mo", "$120/yearly", "$150/yearly"];
+    let selectedPlan = '';
+    let selectedAmount = '';
     if (checkBox.checked) {
       plans.forEach(function (plan, i) {
         let planName = plan.querySelector("#planP").textContent;
         let planPrice = yearlyPrices[i];
         plan.querySelector("#pay").textContent = planPrice;
         paragraphs.forEach((paragraph) => {
-          
+          http:paragraph.style.visibility = "visible";
         });
         if (plan.querySelector(".radio:checked")) {
           selectedPlan = planName;
